644e15556af54e73fbd237f82a4267d33f2fb5e1
bettyrjc <bettyjimenez3010@gmail.com>
2021-04-29 21:21:55 -0400

    :tada: initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text>Holaa!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
